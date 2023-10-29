1ad309e7449aff903e2ee449fe9c963faed76068
Alejjaandro <alejandroolaso98@gmail.com>
2023-10-29 11:27:54 +0100

    Styles corrections

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useParams } from "react-router"
 import { useFetch } from "../hooks/useFetch";
 import { URL_DETAILS, URL_IMAGES, IMDB_URL } from "../settings";
+import './styles/MovieDetails.css';
 
 export default function MovieDetails() {
 
@@ -11,20 +12,23 @@ export default function MovieDetails() {
     useFetch(URL_DETAILS.replace("{movie_id}", movieId), setMovie)
 
     return (
-        <div className="pb-5 min-vh-100">
-            <img className="w-100 h-auto" src={`${URL_IMAGES + movie.backdrop_path}`} alt="" />
+        <div className="pb-5 min-vh-100 text-center">
+
+            <img className="movie-image" src={`${URL_IMAGES + movie.backdrop_path}`} alt="" />
 
             <div className="container">
                 <div className="my-5 text-center">
-                    <h1 className="display-1">{movie.title}</h1>
-                    <span className="ms-3 fs-6 badge bg-primary">{movie.original_language}</span>
-                    <span className="ms-3 fs-6 badge bg-primary">{movie.vote_average}⭐</span>
+                    <h1 className="display-1 fw-bold">{movie.title}</h1>
+                    <span className="ms-3 fs-4 badge bg-primary">{movie.original_language}</span>
+                    <span className="ms-3 fs-4 badge bg-primary">{movie.vote_average}⭐</span>
                 </div>
 
-                <h2>{movie.tagline}</h2>
-                <h6 className="card-subtitle mb-2 text-muted">{movie.release_date}</h6>
-                <p className="my-3">{movie.overview}</p>
-                <a className="btn btn-warning w-25" href={IMDB_URL + movie.imdb_id} target="_blank">IMDb</a>
+                <div className="text-start movie-info">
+                    <h2>{movie.tagline}</h2>
+                    <h6 className="mb-2 text-muted">{movie.release_date}</h6>
+                    <p className="my-3">{movie.overview}</p>
+                    <a className="btn btn-warning w-50" href={IMDB_URL + movie.imdb_id} target="_blank">IMDb</a>
+                </div>
             </div>
         </div>
     )
